76d7af32270397c8be5ee62596d4d9325f2fbccc
Edemone <edem.agbenyo@gmail.com>
2020-03-31 16:22:19 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # covid19-screening
-A simple screening tool for covid19
+A simple app for
+
+1. Getting COVID-19 disease situation by country (tabular and graph)
+2. Self-Diagnose of COVID-19
+3. Communicating to the right entity in case of suspicion of COVID-19
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'index.html'),
+        to: path.resolve(__dirname, 'build')
+      }
+    ]),
+    
+  ]
+};
