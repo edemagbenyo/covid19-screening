6db85ec46ec70af5e411be8fdd205adeb2f5e0dd
Edemone <edem.agbenyo@gmail.com>
2020-04-14 11:18:35 +0000

    Add react with babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import 'bootstrap';
+import "bootstrap";
 
-import './styles/main.scss'
+import "./styles/main.scss";
 import "./ui/countries";
-import "./ui/graph"
+import "./ui/graph";
+import React from "react";
+import { render } from "react-dom";
+
+const div = React.createElement('div',null,'The number are here')
+render(div,document.getElementById('numbers'))
